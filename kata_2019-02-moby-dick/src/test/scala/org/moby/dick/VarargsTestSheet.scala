7cc8e35fe2ed5e52b262842598912092a876853c
Marco Wittig <marco.wittig@jambit.com>
2019-02-20 12:35:53 +0100

    Moby Dick: Fix typo

--- kata_2019-02-moby-dick/src/test/scala/org/moby/dick/VarargsTestSheet.scala
+++ kata_2019-02-moby-dick/src/test/scala/org/moby/dick/VarargsTestSheet.scala
@@ -9,7 +9,7 @@ class VarargsTestSheet extends WordSpec with Matchers {
     "what happen if you want to use a List as method parameter?" in {
       val input = Array("Hello", "World!", "It's", "me", "Mario!")
       // method(input)
-      // This creates a compile error, but see method2
+      // This creates a compile error, but see next test
     }
     "but we want to do it anyway!" in {
       val input = Array("Hello", "World!", "It's", "me", "Mario!")
