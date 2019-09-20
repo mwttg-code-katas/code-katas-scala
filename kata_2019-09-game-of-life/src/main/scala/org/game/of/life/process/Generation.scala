d0ddc3474f6770f27150e9c332efce64f8bf5b69
Marco Wittig <marco.wittig@jambit.com>
2019-09-20 18:44:33 +0200

    Code-Kata: Game of Life
    * Refactoring extract similarities (a loop) (functional style?)

--- kata_2019-09-game-of-life/src/main/scala/org/game/of/life/process/Generation.scala
+++ kata_2019-09-game-of-life/src/main/scala/org/game/of/life/process/Generation.scala
@@ -5,30 +5,61 @@ final case class Cell(x: Int, y: Int)
 object Generation {
   private val IsAlive = '*'
 
-  def zero(lines: List[String], boardSize: BoardSize): Set[Cell] = {
+  def zero(lines: List[String], boardSize: BoardSize): Set[Cell] = forEachCell(boardSize, lines, linesTransformFunc)
+
+  def next(livingCells: Set[Cell], boardSize: BoardSize): Set[Cell] = forEachCell(boardSize, livingCells, nextFunc)
+
+  private def forEachCell[T](boardSize: BoardSize, input: T, f: (Int, Int, T) => Option[Cell]) = {
     val cells =
       for (y <- 0 until boardSize.height;
            x <- 0 until boardSize.width) yield {
-        val character = lines(y).charAt(x)
-        if (character == IsAlive) {
-          Some(Cell(x, y))
-        } else {
-          None
-        }
+        f(x, y, input)
       }
     cells.flatten.toSet
   }
 
-  def next(livingCells: Set[Cell], boardSize: BoardSize): Set[Cell] = {
-    val nextGeneration =
-      for (y <- 0 until boardSize.height;
-           x <- 0 until boardSize.width) yield {
-        val currentCell = Cell(x, y)
-        val neighbours  = neighboursOf(currentCell)
-        applyRules(currentCell, neighbours, livingCells)
-      }
-    nextGeneration.flatten.toSet
+  private def linesTransformFunc(x: Int, y: Int, items: List[String]) = {
+    val character = items(y).charAt(x)
+    if (character == IsAlive) {
+      Some(Cell(x, y))
+    } else {
+      None
+    }
   }
+
+  private def nextFunc(x: Int, y: Int, livingCells: Set[Cell]) = {
+    val currentCell = Cell(x, y)
+    val neighbours  = neighboursOf(currentCell)
+    applyRules(currentCell, neighbours, livingCells)
+  }
+
+  //  All above is the (functional) refactoring for extracting similarities (the loop) of these two function/methods
+  //  not sure if it's better now :-/
+  //
+  //  def zero(lines: List[String], boardSize: BoardSize): Set[Cell] = {
+  //    val cells =
+  //      for (y <- 0 until boardSize.height;
+  //           x <- 0 until boardSize.width) yield {
+  //        val character = lines(y).charAt(x)
+  //        if (character == IsAlive) {
+  //          Some(Cell(x, y))
+  //        } else {
+  //          None
+  //        }
+  //      }
+  //    cells.flatten.toSet
+  //  }
+  //
+  //  def next(livingCells: Set[Cell], boardSize: BoardSize): Set[Cell] = {
+  //    val nextGeneration =
+  //      for (y <- 0 until boardSize.height;
+  //           x <- 0 until boardSize.width) yield {
+  //        val currentCell = Cell(x, y)
+  //        val neighbours  = neighboursOf(currentCell)
+  //        applyRules(currentCell, neighbours, livingCells)
+  //      }
+  //    nextGeneration.flatten.toSet
+  //  }
 
   private def neighboursOf(currentCell: Cell) = {
     val x = currentCell.x
