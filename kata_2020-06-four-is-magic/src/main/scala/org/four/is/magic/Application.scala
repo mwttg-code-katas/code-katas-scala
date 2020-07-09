f67b79008aa926b787a102d70ab3997e9e3eb9d3
Marco Wittig <marco.wittig@jambit.com>
2020-07-09 19:08:43 +0200

    Solve 4 is magic

--- kata_2020-06-four-is-magic/src/main/scala/org/four/is/magic/Application.scala
+++ kata_2020-06-four-is-magic/src/main/scala/org/four/is/magic/Application.scala
@@ -4,6 +4,15 @@ object Application {
 
 
   def main(args: Array[String]): Unit = {
-    // TODO
+    println(FourIsMagic.conjure(1))
+    println(FourIsMagic.conjure(2))
+    println(FourIsMagic.conjure(3))
+    println(FourIsMagic.conjure(4))
+    println(FourIsMagic.conjure(5))
+    println(FourIsMagic.conjure(6))
+    println(FourIsMagic.conjure(7))
+    println(FourIsMagic.conjure(8))
+    println(FourIsMagic.conjure(9))
+    println(FourIsMagic.conjure(123456789153L))
   }
 }
